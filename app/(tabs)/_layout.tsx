2b972cad72591e84a562d92a791f507389f52b4b
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-07-13 12:26:53 +0530

    Gallery Added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,9 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen name="index" options={{ headerTitle: 'Home' }} />
+      <Tabs.Screen name="profile" options={{ headerTitle: 'This is profile' }} />
+      <Tabs.Screen name="gallery" options={{ headerTitle: 'This is gallery' }} />
     </Tabs>
   );
 }
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,43 @@
-import { View, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import { router } from 'expo-router';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 
-export default function Profile() {
-    const router = useRouter();
-
-    return (
-        <View>
-            <Button
-                onPress={() => router.back()}
-                title="Back to Index"
-                color="#841584"
-            />
-        </View>
-    );
+export default function DetailsScreen() {
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <Text>Details</Text>
+      <Button
+  onPress={() => router.back()}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+<FlatList
+        data={DATA}
+        renderItem={({item}) => <Text>{item.title}</Text>}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
